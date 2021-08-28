15d9e62abba76755f315b1db1e2c1ea4bbf08f10
KevinwangNs <89532215+KevinwangNs@users.noreply.github.com>
2021-08-28 16:26:11 +0800

    add image/add receive part

--- RTL/SPI_Master.v
+++ RTL/SPI_Master.v
@@ -19,17 +19,19 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
+ // IP Setting parameter  
+// please set those to meet the requirment of Slave IC datasheet
 module SPI_Master(
+
+
     input c_clk_100m,
     //////Send Inferface
     input i_SPI_Send_Sync,
-    input [31:0] i_SPI_Send_Data,
+    input [TRAN_WIDTH-1:0] i_SPI_Send_Data,
     output reg o_SPI_Send_Over_ack = 0,
     //////Receive Inferface
     output reg o_SPI_Receive_Sync = 0,
-    output reg [31:0]o_SPI_Receive_Data = 0,
+    output reg [TRAN_WIDTH-1:0]o_SPI_Receive_Data = 0,
     /////SPI Inferface
     output reg o_SPI_Clk = 1,
     output reg o_SPI_SS = 1,
@@ -38,16 +40,13 @@ module SPI_Master(
     
     
     );
-    
- // IP Setting parameter  
-// please set those to meet the requirment of Slave IC datasheet
 parameter IDLE_VALUE_for_Clk = 0;
 parameter IDLE_VALUE_for_MOSI = 0;
 parameter DATA_VALID_at_FALLING = 1;//set 0 for RISING EDGE
-parameter[5:0] Tran_width = 24;//8~32
+parameter[5:0] TRAN_WIDTH = 24;//8~32
 
-parameter DE_GLITCH_Enable = 0;//Please DON'T use unless the input signal has strong interference!
-
+parameter Receive_VALID_at_FALLING = 0;
+    
 //Debug port
 reg [31:0] r_Error_Count=0;
 
@@ -74,9 +73,9 @@ reg [4:0]r_Send_SM = s_Sync_wait;
 
 reg r_Send_Sync_last = 0;
 reg [5:0]r_Bit_Index = 0;
-reg [31:0]r_Send_Data_l = 0;
+reg [TRAN_WIDTH-1:0]r_Send_Data_l = 0;
 
-
+reg r_Receive_Data_lock_sig = 0;
 
 always@(posedge c_Clk_Low) begin
     case(r_Send_SM)
@@ -87,6 +86,8 @@ always@(posedge c_Clk_Low) begin
             o_SPI_MOSI <= IDLE_VALUE_for_MOSI;
             
             r_Bit_Index <= 0;
+            o_SPI_Receive_Sync <= o_SPI_Send_Over_ack;
+            r_Receive_Data_lock_sig <= 0;
             
             if(i_SPI_Send_Sync == r_Send_Sync_last)
             begin
@@ -112,7 +113,7 @@ always@(posedge c_Clk_Low) begin
     s_Send_data://Send 1bit and half clock -- Add Bit Index
         begin
             o_SPI_Clk  <= DATA_VALID_at_FALLING;
-            o_SPI_MOSI <= r_Send_Data_l[Tran_width-1];
+            o_SPI_MOSI <= r_Send_Data_l[TRAN_WIDTH-1];
             
             r_Bit_Index <= r_Bit_Index+1;
             
@@ -124,11 +125,13 @@ always@(posedge c_Clk_Low) begin
             o_SPI_Clk <= ~DATA_VALID_at_FALLING;
             r_Send_Data_l <= r_Send_Data_l<<1;           
            
-             if(r_Bit_Index<Tran_width)//check if all bits has been sent
+             if(r_Bit_Index<TRAN_WIDTH)//check if all bits has been sent
             begin
                 r_Send_SM <= s_Send_data;
             end
             else begin
+                r_Receive_Data_lock_sig=1;
+                
                 r_Send_SM <= s_Send_over;
             end
             
@@ -140,6 +143,7 @@ always@(posedge c_Clk_Low) begin
             o_SPI_SS   <= 1'b0;
             o_SPI_MOSI <= IDLE_VALUE_for_MOSI;
             
+            r_Receive_Data_lock_sig = 0;
             o_SPI_Send_Over_ack <= ~o_SPI_Send_Over_ack;
             
             r_Send_SM <= s_Sync_wait;
@@ -152,4 +156,27 @@ always@(posedge c_Clk_Low) begin
          end
     endcase
 end
+
+/////////////Receive Part//////////
+reg [TRAN_WIDTH-1:0]r_SPI_Receive_Data_pre = 0;
+wire w_SPI_Receive_Sample_Clk;
+
+assign w_SPI_Receive_Sample_Clk = Receive_VALID_at_FALLING ? ~o_SPI_Clk : o_SPI_Clk ;
+
+always@(posedge w_SPI_Receive_Sample_Clk)begin
+	if( o_SPI_SS == 0 )begin
+		r_SPI_Receive_Data_pre = i_SPI_MISO + ( r_SPI_Receive_Data_pre << 1 );
+	end
+end
+
+always@(negedge r_Receive_Data_lock_sig)begin
+    o_SPI_Receive_Data <= r_SPI_Receive_Data_pre;
+end
+
+ 
+    
+    
+    
+    
+
 endmodule
--- RTL/SPI_Master_TB.v
+++ RTL/SPI_Master_TB.v
@@ -23,34 +23,54 @@
 module SPI_Master_TB( );
   ///parameters
   parameter c_CLOCK_PERIOD_ns = 10;//100m input
+ 
+ 
+  //change the parameter for different Test!
+   parameter DATA_VALID_at_FALLING_inst0 = 0;
+   parameter DATA_VALID_at_FALLING_inst1 = 0;
+   
+   parameter IDLE_VALUE_for_Clk_inst0 = 0;
+   parameter IDLE_VALUE_for_Clk_inst1 = 0;
+   
+   parameter TRAN_WIDTH_inst0 = 8;
+   parameter TRAN_WIDTH_inst1 = 24;
+   
+   parameter IDLE_VALUE_for_MOSI_inst0 = 0;
+   parameter IDLE_VALUE_for_MOSI_inst1 = 0;
     
     
-    
-   reg  r_Clock =0;   
-   reg  i_SPI_Send_Sync=0;
-   reg[31:0] i_SPI_Send_Data=0;
+   reg  r_Clock = 1'b1;   
+   reg  i_SPI_Send_Sync = 1'b0;
+   reg[31:0] i_SPI_Send_Data = 1'b0;
+   wire w_SPI_MISO_inst0;
+   wire w_SPI_MISO_inst1;
+   reg r_MISO_flop = 1'b0;
+  
    
    wire w_SPI_Send_Over_ack_inst0;
    wire w_SPI_Receive_Sync_inst0;
-   wire w_SPI_Receive_Data_inst0;
+   wire [TRAN_WIDTH_inst0 - 1 : 0 ]w_SPI_Receive_Data_inst0;
    wire w_SPI_Clk_inst0;
    wire w_SPI_SS_inst0;
    wire w_SPI_MOSI_inst0;
    
    wire w_SPI_Send_Over_ack_inst1;
    wire w_SPI_Receive_Sync_inst1;
-   wire w_SPI_Receive_Data_inst1;
+   wire [TRAN_WIDTH_inst1 - 1 : 0 ]w_SPI_Receive_Data_inst1;
    wire w_SPI_Clk_inst1;
    wire w_SPI_SS_inst1;
    wire w_SPI_MOSI_inst1;
    
+   assign w_SPI_MISO_inst0 = r_MISO_flop ? ~w_SPI_MOSI_inst0 : w_SPI_MOSI_inst0;
+   assign w_SPI_MISO_inst1 = r_MISO_flop ? ~w_SPI_MOSI_inst1 : w_SPI_MOSI_inst1;
+
+   
    
    SPI_Master #(
-    .IDLE_VALUE_for_Clk(0),
-    .IDLE_VALUE_for_MOSI(0),
-    .DATA_VALID_at_FALLING(0),
-    .Tran_width(32),
-    .DE_GLITCH_Enable(0)
+    .IDLE_VALUE_for_Clk(IDLE_VALUE_for_Clk_inst0),
+    .IDLE_VALUE_for_MOSI(IDLE_VALUE_for_MOSI_inst0),
+    .DATA_VALID_at_FALLING(DATA_VALID_at_FALLING_inst0),
+    .TRAN_WIDTH(TRAN_WIDTH_inst0)
   ) inst0 (
     .c_clk_100m(r_Clock),
     .i_SPI_Send_Sync(i_SPI_Send_Sync),
@@ -61,15 +81,14 @@ module SPI_Master_TB( );
     .o_SPI_Clk(w_SPI_Clk_inst0),
     .o_SPI_SS(w_SPI_SS_inst0),
     .o_SPI_MOSI(w_SPI_MOSI_inst0),
-    .i_SPI_MISO()
+    .i_SPI_MISO(w_SPI_MISO_inst0)
   );
   
    SPI_Master #(
-    .IDLE_VALUE_for_Clk(0),
-    .IDLE_VALUE_for_MOSI(0),
-    .DATA_VALID_at_FALLING(1),
-    .Tran_width(24),
-    .DE_GLITCH_Enable(0)
+    .IDLE_VALUE_for_Clk(IDLE_VALUE_for_Clk_inst1),
+    .IDLE_VALUE_for_MOSI(IDLE_VALUE_for_MOSI_inst1),
+    .DATA_VALID_at_FALLING(DATA_VALID_at_FALLING_inst1),
+    .TRAN_WIDTH(TRAN_WIDTH_inst1)
   ) inst1 (
     .c_clk_100m(r_Clock),
     .i_SPI_Send_Sync(i_SPI_Send_Sync),
@@ -80,7 +99,7 @@ module SPI_Master_TB( );
     .o_SPI_Clk(w_SPI_Clk_inst1),
     .o_SPI_SS(w_SPI_SS_inst1),
     .o_SPI_MOSI(w_SPI_MOSI_inst1),
-    .i_SPI_MISO()
+    .i_SPI_MISO(w_SPI_MISO_inst1)
   );
    
   always
@@ -89,11 +108,14 @@ module SPI_Master_TB( );
 // Main Testing:
 initial
     begin
-        #100
-        i_SPI_Send_Data=32'hffffa5a5;
-        #100
-        i_SPI_Send_Sync=1;
-       
+        # 90
+        i_SPI_Send_Data=32'h8080a5a5;
+        # 10
+        i_SPI_Send_Sync= 1'b1;       //make Send 
+        # 1500
+        i_SPI_Send_Sync=~i_SPI_Send_Sync;//make Send 
+        r_MISO_flop= 1'b1;
+        
     end
     
     
